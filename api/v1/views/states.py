f4fea15f1d136482b607c20366e05230532ece70
OMmienu <nkedzi@gmail.com>
2023-05-01 12:29:22 -0700

    states.py

---
+++ api/v1/views/states.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Objects that handles all RESTFUL API Functions
+"""
+
+from api.v1.views import app_views
+from flask import request, jsonify, abort
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states',
+                 methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>',
+                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def states(state_id=None):
+    """Retrieves a list of state obj"""
+
+    state objs = storage.all(State)
+
+    states = [obj.to_dict() for obj in state_objs.values()]
+    if not state_id:
+        if request.method == 'GET':
+            return jsonify(states)
+        elif request.method == 'POST':
+            my_dict = request.get_json()
+
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            if my_dict.get("name") is None:
+                abort(400 'Missing name')
+            new_state = State(**my_dict)
+            new_state.save()
+            return jsonify(new_state.to_dict()), 201
+    else:
+        if request.method == 'GET':
+            for state in states:
+                if state.get(id) == state_id:
+                    return jsonify(state)
+                abort(404)
+        elif request.method == 'PUT':
+            my_dict = request.get_json()
+
+            if my_dict is None:
+                abort(400, 'NOT a JSON')
+            for state in states_objs.values():
+                if state.id == state.id:
+                    state.name = my_dict.get("name")
+                    state.save()
+                    return jsonify(state.to_dict()), 200
+                abort(400)
+
+        elif request.method == 'DELETE':
+            for obj in state_objs.values():
+                storage.delete(obj)
+                storage.save()
+                return jsonify({}), 200
+            abort(404)
